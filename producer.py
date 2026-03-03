a078791b60b911464d303b18321444dc3597de46
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2026-03-02 21:18:49 -0600

    Setting up Callback function

--- producer.py
+++ producer.py
@@ -10,6 +10,14 @@ producer_config = {
 
 producer = Producer  (producer_config)
 
+def delivery_report(err,msg):
+    if err:
+        print(f"Delivery failed: {err}")
+
+    else:
+        print(f"Delivered {msg.vaule().decode("utf-8")}")    
+    
+
 ## This is what an event looks like . Its a Json object
 order = {
     "order_id": str(uuid.uuid4()),
@@ -17,5 +25,13 @@ order = {
     "item": "frozen yogurt",
     "quantity": 10
 }
+## This code turns the Json into a strng  and then to Bytes so Kafak can read it 
+vaule = json.dumps(order).encode("utf-8")
 
-json.dumps(order).encode("utf-8")
+producer.produce(
+        topic="orders",
+        vaule=value,
+        callback=delivery_report)
+
+#Good for clean code. Send Kafka order in batches if there alot. If it crashes the Flush forces them to send 
+producer.flush()
