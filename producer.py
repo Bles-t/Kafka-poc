38a7cc0abe33bc37726b20245dab73a873ec2b81
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2026-03-02 21:00:44 -0600

    Setting up Json to covert

--- producer.py
+++ producer.py
@@ -1,2 +1,21 @@
+import json
+import uuid
+
+
 from confluent_kafka import Producer
 
+producer_config = {
+    "bootstrap.servers": "localhost:9092"
+}
+
+producer = Producer  (producer_config)
+
+## This is what an event looks like . Its a Json object
+order = {
+    "order_id": str(uuid.uuid4()),
+    "user": "lara",
+    "item": "frozen yogurt",
+    "quantity": 10
+}
+
+json.dumps(order).encode("utf-8")
